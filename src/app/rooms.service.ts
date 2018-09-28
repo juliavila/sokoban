9005c97fb930f3767d937cdc08ef21ef5c221ed9
Julia Ferreira Vila <julia.fvila@hotmail.com>
2018-09-28 13:03:38 -0300

    adding new levels

--- src/app/rooms.service.ts
+++ src/app/rooms.service.ts
@@ -1,9 +1,8 @@
-import { Injectable, OnInit } from '@angular/core';
-import { RoomModule } from './shared/modules/room.module';
-import { TileModule } from './shared/modules/tile.module';
+import { Injectable } from '@angular/core';
 import { TILE_TYPE } from './shared/enums/tile-type.enum';
 import { Coordinate } from './shared/modules/coordinate.module';
-import * as _ from 'lodash';
+import { RoomModule } from './shared/modules/room.module';
+import { TileModule } from './shared/modules/tile.module';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +12,16 @@ export class RoomsService {
   private rooms = new Array<RoomModule>();
   private index: number;
 
-  constructor() { 
+  constructor() {
 
-    let wall          = new TileModule (TILE_TYPE.WALL, false, false);
-    let ground        = new TileModule (TILE_TYPE.GROUND, false, false);
-    let groundBox     = new TileModule (TILE_TYPE.GROUND, false, true);
-    let groundMark    = new TileModule (TILE_TYPE.GROUND, true, false);
-    let groundMarkBox = new TileModule (TILE_TYPE.GROUND, true, true);
+    const wall          = new TileModule (TILE_TYPE.WALL, false, false);
+    const ground        = new TileModule (TILE_TYPE.GROUND, false, false);
+    const groundBox     = new TileModule (TILE_TYPE.GROUND, false, true);
+    const groundMark    = new TileModule (TILE_TYPE.GROUND, true, false);
+    const groundMarkBox = new TileModule (TILE_TYPE.GROUND, true, true);
 
-    this.rooms.push( { 
-      name: "1 - Genesis",
+    this.rooms.push( {
+      name: '1 - Genesis',
       tiles: [
         [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}],
         [ {...wall}, {...ground}, {...ground}, {...ground}, {...wall}, {...wall} ],
@@ -34,17 +33,45 @@ export class RoomsService {
       cursor: new Coordinate(1, 1)
     } );
 
-    this.rooms.push( { 
-      name: "2 - Genesis",
+    this.rooms.push( {
+      name: '2',
       tiles: [
-        [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}],
-        [ {...wall}, {...ground}, {...ground}, {...ground}, {...wall}, {...wall} ],
-        [ {...wall}, {...groundMark}, {...groundMarkBox}, {...groundBox}, {...ground}, {...wall} ],
-        [ {...wall}, {...ground}, {...ground}, {...wall}, {...ground}, {...wall}],
-        [ {...wall}, {...ground}, {...ground}, {...ground}, {...ground}, {...wall} ],
-        [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall} ]
+        [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}],
+        [ {...wall}, {...ground}, {...ground}, {...ground}, {...ground}, {...wall}, {...wall} ],
+        [ {...wall}, {...ground}, {...groundBox}, {...groundBox}, {...ground}, {...ground}, {...wall} ],
+        [ {...wall}, {...ground}, {...wall}, {...groundMark}, {...ground}, {...groundMark}, {...wall}],
+        [ {...wall}, {...ground}, {...ground}, {...ground}, {...ground}, {...ground}, {...wall} ],
+        [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall} ]
       ],
       cursor: new Coordinate(1, 1)
+    } );
+
+    this.rooms.push( {
+      name: '3',
+      tiles: [
+        [ {...ground}, {...ground}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}],
+        [ {...wall}, {...wall}, {...wall}, {...ground}, {...ground}, {...wall}, {...ground}],
+        [ {...wall}, {...ground}, {...ground}, {...groundMark}, {...groundBox}, {...wall}, {...wall} ],
+        [ {...wall}, {...ground}, {...ground}, {...ground}, {...groundBox}, {...ground}, {...wall} ],
+        [ {...wall}, {...ground}, {...wall}, {...groundMark}, {...ground}, {...ground}, {...wall}],
+        [ {...wall}, {...ground}, {...ground}, {...ground}, {...ground}, {...ground}, {...wall} ],
+        [ {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall} ]
+      ],
+      cursor: new Coordinate(1, 2)
+    } );
+
+    this.rooms.push( {
+      name: '4',
+      tiles: [
+        [ {...ground}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}],
+        [ {...wall}, {...wall}, {...ground}, {...ground}, {...ground}, {...wall}, {...wall}],
+        [ {...wall}, {...ground}, {...ground}, {...wall}, {...ground}, {...ground}, {...wall} ],
+        [ {...wall}, {...ground}, {...groundBox}, {...groundMarkBox}, {...groundBox}, {...ground}, {...wall} ],
+        [ {...wall}, {...ground}, {...ground}, {...groundMark}, {...ground}, {...ground}, {...wall} ],
+        [ {...wall}, {...wall}, {...ground}, {...groundMark}, {...ground}, {...wall}, {...wall}],
+        [ {...ground}, {...wall}, {...wall}, {...wall}, {...wall}, {...wall}, {...ground}]
+      ],
+      cursor: new Coordinate(2, 1)
     } );
 
     this.index = 0;
@@ -57,7 +84,7 @@ export class RoomsService {
   }
 
   getNextRoom() {
-    this.index = (this.index === this.rooms.length - 1) ? 0 : this.index + 1; 
+    this.index = (this.index === this.rooms.length - 1) ? 0 : this.index + 1;
     return this.getCurrentRoom();
   }
 
--- src/app/tile/tile.module.spec.ts
+++ src/app/tile/tile.module.spec.ts
@@ -1,11 +1,11 @@
-import { TileModule } from '../shared/modules/tile.module';
 import { TILE_TYPE } from '../shared/enums/tile-type.enum';
+import { TileModule } from '../shared/modules/tile.module';
 
 describe('TileModule', () => {
   let tileModule: TileModule;
 
   beforeEach(() => {
-    tileModule = new TileModule(TILE_TYPE.GROUND, true, false, false);
+    tileModule = new TileModule(TILE_TYPE.GROUND, true, false);
   });
 
   it('should create an instance', () => {
